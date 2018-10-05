8f79aeab3e6fb032c6f6d5a390562b7b95e7d669
nataliakikuchi <nataliakikuchi90@gmail.com>
2018-10-05 12:00:10 -0300

    adicionamos eventos no input

--- exercicioNumeros/script.js
+++ exercicioNumeros/script.js
@@ -7,12 +7,27 @@ var button = document.querySelector("#btn");
 
 button.addEventListener("click", function(){
   var value = input.value;
-  if(input.value == numeroSorteado){
+//console.log(value);
+//string vazia é false. ! é a negação ->se não tiver nada no valor do input
+  if(!value){
+    alert("Insira um número");
+    //para a execução da função
+    return;
+  }
+  if(value == numeroSorteado){
     alert("Você acertou");
   }else{
     alert("Você errou");
   }
 });
+//a função como parâmetro será executada toda vez que o evento ocorrer no elemento input
+input.addEventListener("keyup", function(){
+ var value = input.value;
+ //se o value é um não número, limpar o input
+if(window.isNaN(value)){
+  input.value = "";
+}
+})
 
 //checa se o botão realmente existe para executar as ações
 //if(btn !== null){
