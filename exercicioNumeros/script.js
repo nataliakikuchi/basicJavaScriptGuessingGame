3f46df5d5d3718e78d9992085173c39eaea24cde
nataliakikuchi <nataliakikuchi90@gmail.com>
2018-10-24 15:20:02 -0300

    Adição de estilo e mais lógica em JS

--- exercicioNumeros/script.js
+++ exercicioNumeros/script.js
@@ -1,59 +1,59 @@
-var numeroSorteado = 8;
 
-var input = document.querySelector("#primeiro-input");
-console.dir(input.value);
+(function() {
+  var numeroSorteado = 8;
 
-var button = document.querySelector("#btn");
-var regraDeContagem = 3;
-var clickDoUsuario = 0;
+  var input = document.querySelector("#first-input");
+  console.dir(input.value);
 
-button.addEventListener("click", function(){
-  var value = input.value;
-//console.log(value);
-//string vazia é false. ! é a negação ->se não tiver nada no valor do input
-  if(!value){
+  var button = document.querySelector("#btn-play");
+  var regraDeContagem = 3;
+  var clickDoUsuario = 0;
+
+  function avisaQueNaoEhUmNumero() {
     alert("Insira um número");
-    //para a execução da função
-    return;
   }
-  if(value == numeroSorteado){
-    alert("Você acertou");
-    clickDoUsuario = 0;
+
+  button.addEventListener("click", function(){
+    var value = input.value;
+
+    if(!value){
+      avisaQueNaoEhUmNumero();
+      return;
+    }
+    if(value == numeroSorteado){
+      alert("Você acertou");
+      clickDoUsuario = 0;
+      input.value = "";
+      input.focus();
+    }else{
+      alert("Você errou");
+      input.value = "";
+      input.focus();
+    }
+
+    clickDoUsuario = clickDoUsuario + 1;
+    console.log(clickDoUsuario);
+    if(clickDoUsuario >= 3){
+      button.disabled = true;
+      input.disabled = true;
+    }
+  });
+
+  input.addEventListener("keyup", function(){
+   var value = input.value;
+
+  if(window.isNaN(value)){
     input.value = "";
-    input.focus();
-  }else{
-    alert("Você errou");
-    input.value = "";
-    input.focus();
   }
+  })
 
-  clickDoUsuario = clickDoUsuario + 1;
-  console.log(clickDoUsuario);
-  if(clickDoUsuario >= 3){
-    button.disabled = true;
-    input.disabled = true;
-  }
-});
+  var tryAgain = document.querySelector("#btn-reset");
+  tryAgain.addEventListener("click", function(){
+    if(clickDoUsuario == 3){
+      clickDoUsuario = 0;
+      button.disabled = false;
+      input.disabled = false;
+    }
+  })
 
-
-//a função como parâmetro será executada toda vez que o evento ocorrer no elemento input
-input.addEventListener("keyup", function(){
- var value = input.value;
- //se o value é um não número, limpar o input
-if(window.isNaN(value)){
-  input.value = "";
-}
-})
-
-//checa se o botão realmente existe para executar as ações
-//if(btn !== null){
-//}
-
-var tryAgain = document.querySelector("#reset");
-tryAgain.addEventListener("click", function(){
-  if(clickDoUsuario == 3){
-  clickDoUsuario = 0;
-  button.disabled = false;
-  input.disabled = false;
-}
-})
+})();
