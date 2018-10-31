9b209539dd3e97637e3344959ca9bac90909dccb
nataliakikuchi <nataliakikuchi90@gmail.com>
2018-10-31 07:39:57 -0300

    Acréscimo de lógica

--- exercicioNumeros/script.js
+++ exercicioNumeros/script.js
@@ -1,6 +1,6 @@
 
 (function() {
-  var numeroSorteado = 8;
+  var numeroSorteado = Math.round(Math.random() * 10);
 
   var input = document.querySelector("#first-input");
   console.dir(input.value);
@@ -9,16 +9,37 @@
   var regraDeContagem = 3;
   var clickDoUsuario = 0;
 
+  var message = document.querySelector("#message");
+
+  var numeroDeVezesQuePerdeu = 0;
+  atualizaPerdeu();
+
   function avisaQueNaoEhUmNumero() {
-    alert("Insira um número");
+    message.innerHTML = "Insira um número válido para jogar!"
   }
 
   function acertou() {
-    alert("Você acertou");
+    message.innerHTML = "Você acertou, o número oculto é " + numeroSorteado + "!"
   }
 
   function errou() {
-    alert("Você errou");
+    var chances = regraDeContagem - clickDoUsuario;
+    if(chances == 2) {
+      message.innerHTML = "Você errou, mas ainda tem mais " + chances + " chances!"
+    }else if(chances == 1) {
+        message.innerHTML = "Você errou, mas ainda tem mais " + chances + " chance!"
+    }else{
+        message.innerHTML = "Você errou! O número oculto era " + numeroSorteado + "!"
+    }
+  }
+
+  function gerarNovoNumero() {
+    numeroSorteado = Math.round(Math.random() * 10);
+  }
+
+  function atualizaPerdeu() {
+    var vezesQuePerdeu = document.querySelector("#perdeu");
+    vezesQuePerdeu.innerHTML = numeroDeVezesQuePerdeu;
   }
 
   button.addEventListener("click", function(){
@@ -28,22 +49,25 @@
       avisaQueNaoEhUmNumero();
       return;
     }
+    clickDoUsuario = clickDoUsuario + 1;
     if(value == numeroSorteado){
       acertou();
       clickDoUsuario = 0;
       input.value = "";
       input.focus();
+      gerarNovoNumero();
     }else{
       errou();
       input.value = "";
       input.focus();
     }
 
-    clickDoUsuario = clickDoUsuario + 1;
     console.log(clickDoUsuario);
     if(clickDoUsuario >= 3){
       button.disabled = true;
       input.disabled = true;
+      numeroDeVezesQuePerdeu = numeroDeVezesQuePerdeu + 1;
+      atualizaPerdeu();
     }
   });
 
@@ -51,16 +75,24 @@
    var value = input.value;
 
   if(window.isNaN(value)){
-    input.value = "";
+    //input.value = "";
+      message.innerHTML = "Insira um número válido para jogar!"
+      button.disabled = true;
+  }
+  else{
+    button.disabled = false;
+    message.innerHTML = "";
   }
   })
 
   var tryAgain = document.querySelector("#btn-reset");
   tryAgain.addEventListener("click", function(){
+    message.innerHTML = "";
     if(clickDoUsuario == 3){
       clickDoUsuario = 0;
       button.disabled = false;
       input.disabled = false;
+      gerarNovoNumero();
     }
   })
 
